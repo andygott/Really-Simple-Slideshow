1c49a066ac25e9b9042aadea0368dec3c05b7b82
Andy <andy@reallysimpleworks.com>
2011-04-28 11:33:12 +0100

    --amend

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,26 +4,6 @@
 
 $(document).ready(function () {
 
-	$.rsfSlideshow({interval: 1, transition: 300});
-	
 	$('.rs-slideshow').rsfSlideshow();
-	
-	/*$('#slideshow').bind('rsPostTransition', function(e, slide) {
-		var html = '<h3>A transition has completed!</h3>';
-		html += '<p>Slide data:</p><ul>';
-		for (var key in slide) {
-			html += '<li>' + key + ': ' + slide[key] + '</li>';
-		}
-		html += '</ul>';
-		$('#callback-messages').html(html); 
-	});
-	$('#slideshow').bind('rsFirstSlide', function(slide) {
-		$('#callback-messages').append('<p><strong>This is the first slide</strong></p>'); 
-	});
-	$('#slideshow').bind('rsLastSlide', function(slide) {
-		$('#callback-messages').append('<p><strong>This is the last slide</strong></p>'); 
-	});*/
-	
-	
 	
 });
